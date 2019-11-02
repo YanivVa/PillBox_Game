d3b435cc62184e21909440b217fa4d313e66c583
vintifer <zil.eran@gmail.com>
2019-11-02 16:45:10 +0200

    added on quit popup

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject popup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onQuit()
+    {
+        popup.SetActive(true);
+    }
+
+    public void onCancel()
+    {
+        popup.SetActive(false);
+    }
+
+    public void onApprove()
+    {
+        SceneManager.LoadScene("Scenes/Opening");
+    }
+}
