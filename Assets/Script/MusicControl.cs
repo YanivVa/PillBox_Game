4b65563ad9d7f96b9a8ea1355d57d546a8bce208
= <elad19892012@gmail.com>
2019-11-03 19:03:18 +0200

    fixMusicManager

--- Assets/Script/MusicControl.cs
+++ Assets/Script/MusicControl.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicControl : MonoBehaviour
-{
-    public AudioClip otherClip;
+public class MusicControl : MonoBehaviour{
     AudioSource audioSource;
-
+    int i = 0;
+    
     // Start is called before the first frame update
     void Start(){
         audioSource = GetComponent<AudioSource>();
-
     }
 
     // Update is called once per frame
     void Update(){
-        if (!audioSource.isPlaying){
-            audioSource.clip = otherClip;
-            audioSource.Play();
-        }
+       
 
     }
-    public void pause(){
+    public void playAndPause(){
 
-
+        if (i == 0){
+            audioSource.Pause();
+            i = i + 1;
+        }
+        else{
+            audioSource.Play();
+            i = i - 1;
+        }
     }
 }
